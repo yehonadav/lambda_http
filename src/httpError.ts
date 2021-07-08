d941c19111b50725ff8e4e90dccdb75b4929e4e4
yonadav <yonadav.barilan@gmail.com>
2021-07-09 01:02:12 +0300

    update

--- src/httpError.ts
+++ src/httpError.ts
@@ -5,10 +5,12 @@ export interface IHttpError {
 
 export class HttpError extends Error {
   statusCode: number;
+  message: string;
 
   constructor({statusCode, message}:IHttpError) {
     super(message);
     this.statusCode = statusCode;
+    this.message = message;
   }
 }
 
